c23c5c6c2ec8f791d253322a9fe25d3079ade188
Ben Fulton <fulton.benjamin@gmail.com>
2019-06-25 09:53:02 -0400

    Split custom click args/options out

---
+++ click_custom.py
@@ -0,0 +1,82 @@
+from click import Argument, Option, Choice, Path
+
+
+class SourceArgument(Argument):
+    """A custom argument that accepts either a valid file path string or
+    the name of a downloadable package list for the specified type."""
+
+    def __init__(self, *args, **kwargs):
+        # choices is of type dict(str: list), mapping types to lists of
+        # names of downloadable package lists
+        self.sources = kwargs.pop('sources')
+
+        # init a choice of either <path> or <source>; this Choice will
+        # be overriden by handle_parse_result()
+        kwargs['type'] = Choice(['<path>', '<source>'])
+        super().__init__(*args, **kwargs)
+
+    def handle_parse_result(self, ctx, opts: dict, args: list):
+        if '.' in opts['source']:
+            # source is likely a file path
+            self.type = Path(exists=True)
+            return super().handle_parse_result(ctx, opts, args)
+        else:
+            # source is likely a web list
+            self.type = Choice(self.sources[opts['type_']])
+            return super().handle_parse_result(ctx, opts, args)
+
+
+class SortOption(Option):
+    """
+    A custom option that accepts an asc/desc value followed by any
+    number of sort criteria.
+
+    This code is loosely based on this example:
+    https://stackoverflow.com/questions/48391777/nargs-equivalent-for-options-in-click
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.original_process = None
+        super().__init__(*args, **kwargs)
+
+    def add_to_parser(self, parser, ctx):
+        """Parses an unlimited number of sort args by overriding the
+        default add_to_parser() function and pulling additional params
+        off the command line until a new option (beginning with '-')
+        is encountered."""
+
+        def override_process(value: str, state):
+            """Overrides the original parser.process and loops over the
+            remaining args, adding them to the values for the --sort
+            option until a new option is encountered."""
+
+            # grab everything up to the next option (starts with '-')
+            value = [value]
+            # state.rargs contains *all* remaining args
+            while state.rargs and not state.rargs[0].startswith('-'):
+                value.append(state.rargs.pop(0))
+
+            # call the original parser process()
+            self.original_process(value, state)
+
+        # first call the original add_to_process to add the --sort
+        # option to the list of parsed options
+        super().add_to_parser(parser, ctx)
+
+        # get the original parser process for the --sort option
+        name = self.opts[0]
+        # long_opt or short_opt supports both -l and --long options
+        parser = parser._long_opt.get(name) or \
+                 parser._short_opt.get(name)
+
+        # store the original parser.process...
+        self.original_process = parser.process
+        # ... and replace it with our own
+        parser.process = override_process
+
+    def handle_parse_result(self, ctx, opts: dict, args: list):
+        # convert sort opt from list(list()) to list(); needed due to
+        # overriding the option's default add_to_parser()
+        if opts.get('sort'):
+            opts['sort'] = opts['sort'][0]
+        return super().handle_parse_result(ctx, opts, args)
--- inputset.py
+++ inputset.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-from click import Argument, Choice, Option, Path, argument, command, option
+from click import Choice, argument, command, option
+from click_custom import SourceArgument, SortOption
 
 types = {
     'npm': {
@@ -22,72 +23,12 @@ types = {
 sources = {t: [s for s in data['lists']] for t, data in types.items()}
 
 
-class SourceArgument(Argument):
-    """A custom argument that accepts either a valid file path string or
-    the name of a downloadable package list for the specified type."""
-
-    def __init__(self, *args, **kwargs):
-        # init a choice of either <path> or <source>; this Choice will
-        # be overriden by handle_parse_result()
-        kwargs['type'] = Choice(['<path>', '<source>'])
-        super().__init__(*args, **kwargs)
-
-    def handle_parse_result(self, ctx, opts: dict, args: list):
-        if '.' in opts['source']:
-            # source is likely a file path
-            self.type = Path(exists=True)
-            return super().handle_parse_result(ctx, opts, args)
-        else:
-            # source is likely a web list
-            self.type = Choice(sources[opts['type_']])
-            return super().handle_parse_result(ctx, opts, args)
-
-
-class SortOption(Option):
-    """A custom option that accepts an asc/desc value followed by any
-    number of sort criteria."""
-
-    def __init__(self, *args, **kwargs):
-        self.parser_process = None
-
-        kwargs['type'] = Choice(['asc', 'desc', 'popularity', 'date', 'name'])
-        kwargs['multiple'] = True
-        super().__init__(*args, **kwargs)
-
-    def add_to_parser(self, parser, ctx):
-        """Parses an unlimited number of sort args."""
-        # Simplified version of: https://stackoverflow.com/questions/48391777/nargs-equivalent-for-options-in-click
-        def parser_process(value, state):
-            # grab everything up to the next option
-            value = [value]
-            while state.rargs and not state.rargs[0].startswith('-'):
-                value.append(state.rargs.pop(0))
-
-            # call the actual process()
-            self.parser_process(value, state)
-
-        super().add_to_parser(parser, ctx)
-
-        # do some magic...
-        name = self.opts[0]
-        our_parser = parser._long_opt.get(name) or \
-                     parser._short_opt.get(name)
-        our_parser.action = 'append'
-        self.parser_process = our_parser.process
-        our_parser.process = parser_process
-
-    def handle_parse_result(self, ctx, opts: dict, args: list):
-        # convert sort opt from list(list()) to list()
-        if opts.get('sort'):
-            opts['sort'] = opts['sort'][0]
-        return super().handle_parse_result(ctx, opts, args)
-
-
 @command()
 @argument('type_', type=Choice(types.keys()))
-@argument('source', cls=SourceArgument)
+@argument('source', cls=SourceArgument, sources=sources)
 @option('--get', type=Choice(['latest', 'major', 'all']))
-@option('--sort', cls=SortOption)
+@option('--sort', cls=SortOption, multiple=True,
+        type=Choice(['asc', 'desc', 'popularity', 'date', 'name']))
 @option('--head', type=int)
 @option('--sample', type=int)
 def generate_inputset(type_: str, source: str, get: str, sort: tuple,
