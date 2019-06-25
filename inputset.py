cd6e83fb6d03fdf234ac1f9267a45c9f2dd87b61
Ben Fulton <fulton.benjamin@gmail.com>
2019-06-25 11:30:36 -0400

    Add help text to click commands

--- inputset.py
+++ inputset.py
@@ -27,17 +27,28 @@ sources = {t: [s for s in data['lists']] for t, data in registries.items()}
 @command()
 @argument('registry', type=Choice(registries.keys()))
 @argument('source', cls=SourceArgument, sources=sources)
-@option('--get', type=Choice(['latest', 'major', 'all']))
-@option('--sort', cls=SortOption, multiple=True, help='Optional',
-        type=Choice(['asc', 'desc', 'popularity', 'date', 'name']))
-@option('--head', type=int, help='Optional')
-@option('--sample', type=int, help='Optional')
-@option('--name', prompt='Filename')
-@option('--version', prompt='Version')
-@option('--description', prompt='Description', default='', help='Optional')
-@option('--readme', prompt='Readme', default='', help='Optional')
-@option('--author', prompt='Author', default=get_user_name, help='Defaults to git user.name')
-@option('--email', prompt='Email', default=get_user_email, help='Defaults to git user.email')
+@option('--get', type=Choice(['latest', 'major', 'all']), default='all',
+        help='Which versions/commits to obtain. Defaults to all.')
+@option('--sort', cls=SortOption, multiple=True,
+        type=Choice(['asc', 'desc', 'popularity', 'date', 'name']),
+        help='OPTIONAL Sort the list of versions/commits, performed '
+             'immediately after --get.')
+@option('--head', type=int,
+        help='OPTIONAL Trim to the first n projects.')
+@option('--sample', type=int,
+        help='OPTIONAL Randomly sample n projects.')
+@option('--name', prompt='Filename',
+        help='Input set name and file name.')
+@option('--version', prompt='Version',
+        help='Input set version (uses semantic versioning).')
+@option('--description', prompt='Description', default='',
+        help='OPTIONAL Input set description.')
+@option('--readme', prompt='Readme', default='',
+        help='OPTIONAL Input set markdown readme.')
+@option('--author', prompt='Author', default=get_user_name,
+        help='Author name. Defaults to git user.name.')
+@option('--email', prompt='Email', default=get_user_email,
+        help='Author email. Defaults to git user.email.')
 def generate_inputset(registry: str, source: str,
                       get: str, sort: tuple,
                       head: int, sample: int,
@@ -45,7 +56,7 @@ def generate_inputset(registry: str, source: str,
                       description: str, readme: str,
                       author: str, email: str):
     """Generate an input set from one of the named source types."""
-    temp = 5
+    print('Success!!!')
     pass
 
     """
