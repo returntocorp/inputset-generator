3162bbb09a44c130b3e6b3e4b18253c59d9facb2
Ben Fulton <fulton.benjamin@gmail.com>
2019-06-28 19:27:07 -0400

    Skeletoned out other registries

--- registries/npm.py
+++ registries/npm.py
@@ -2,19 +2,18 @@ from registries import Registry
 from structures import Dataset, Project
 
 
-class GithubRegistry(Registry):
+class NpmRegistry(Registry):
     def __init__(self):
         super().__init__()
 
         # set project registry name and url format
-        self.name = 'github'
+        self.name = 'npm'
         self.url_format = '???'
 
         # add pypi-specific weblists
-        # see: https://stackoverflow.com/questions/19855552/how-to-find-out-the-most-popular-repositories-on-github
         self.weblists.update({
-            '1kstarred': {
-                'url': 'https://api.github.com/search/repositories?q=stars%3A%3E0&sort=stars&per_page=100',
+            '???': {
+                'url': '???',
                 'parser': 'self._???'
             }
         })
