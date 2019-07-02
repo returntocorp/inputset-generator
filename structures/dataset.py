8df5933d09851a4f5fa77c36e16793e10f91296f
Ben Fulton <fulton.benjamin@gmail.com>
2019-07-02 01:24:52 -0400

    Dataset changes

--- structures/dataset.py
+++ structures/dataset.py
@@ -1,11 +1,13 @@
 from pathlib import Path
-from typing import List
+from typing import List, Optional
+from types import MethodType
 
 from .project import Project
 
 
 class Dataset:
-    def __init__(self, registry: str = None):
+    def __init__(self):
+        from functions import functions
         from util import get_user_name, get_user_email
 
         # a dataset contains projects
@@ -13,16 +15,35 @@ class Dataset:
 
         # registry is used to access web resources
         self.registry = None
-        if registry:
-            self.set_registry(registry)
 
-        # dataset metadata
+        # register the various transformation functions
+        for name, function in functions.items():
+            setattr(self, name, MethodType(function, self))
+
+        # set default dataset metadata
         self.name = None
         self.version = None
         self.description = None
         self.readme = None
         self.author = get_user_name()
         self.email = get_user_email()
+
+    def set_meta(self, name=None, version=None, description=None,
+                 readme=None, author=None, email=None):
+        """Sets dataset metadata."""
+        if not (name or version or description
+                or readme or author or email):
+            raise Exception('Error setting metadata. Must provide at '
+                            'least one of name, version, description, '
+                            'readme, author, or email.')
+
+        # override existing data only if the override is not None
+        self.name = name or self.name
+        self.version = version or self.version
+        self.description = description or self.description
+        self.readme = readme or self.readme
+        self.author = author or self.author
+        self.email = email or self.email
 
     def set_registry(self, registry: str) -> None:
         """Loads up the specified registry."""
@@ -36,11 +57,9 @@ class Dataset:
         # link to the appropriate registry
         self.registry = registries[registry]
 
-    def load_file(self, path: str):
+    def load_file(self, path: str) -> None:
         """Uses a file handler to load a dataset from file."""
         from file_handlers import file_handlers
-
-        print('Loading %s' % path)
 
         # check if the path is valid
         if not Path(path).is_file():
@@ -53,34 +72,54 @@ class Dataset:
                             "are: %s." % (extension, list(file_handlers)))
 
         # load initial data from the file
-        file = file_handlers[extension]
-        file.load(self, path)
+        print('Loading %s...' % path)
+        file_handlers[extension].load(self, path)
 
-    def load_weblist(self, name: str):
+    def load_weblist(self, name: str) -> None:
         """Loads a weblist from the registry."""
         from registries import registries
 
-        print('Loading %s %s' % (self.registry.name, name))
+        # check if the registry has been set
+        if not self.registry:
+            raise Exception('Registry has not been set. Valid '
+                            'registries are: %s' % list(registries))
+
+        # check if the name is valid
+        names = list(self.registry.weblists)
+        if name not in names:
+            raise Exception('Invalid weblist for registry %s. Valid '
+                            'weblists are: %s' % (self.registry.name,
+                                                  names))
+
+        # load initial data from the weblist
+        print("Loading '%s' from %s..." % (name, self.registry.name))
+        self.registry.load_weblist(self, name)
+
+    def load_details(self) -> None:
+        """Downloads all projects' metadata."""
+        from registries import registries
 
         # check if the registry has been set
         if not self.registry:
-            raise Exception('Registry has not been set. Valid types '
-                            'are: %s' % list(registries))
+            raise Exception('Registry has not been set. Valid '
+                            'registries are: %s' % list(registries))
 
-        # check if the name is valid
-        list_names = list(self.registry.weblists)
-        if name not in list_names:
-            reg_name = self.registry.name
-            raise Exception('Invalid weblist for registry %s. Valid '
-                            'names are: %s' % (reg_name, list_names))
-
-        # load initial data from the weblist
-        self.registry.load_weblist(self, name)
-
-    def load_projects(self):
-        """Downloads all projects' metadata."""
         for project in self.projects:
-            self.registry.load_project(project)
+            print("Retrieving details on %s..." % project)
+            self.registry.load_details(project)
+
+    def load_versions(self, historical: str):
+        """Downloads all projects' versions."""
+        from registries import registries
+
+        # check if the registry has been set
+        if not self.registry:
+            raise Exception('Registry has not been set. Valid '
+                            'registries are: %s' % list(registries))
+
+        for project in self.projects:
+            print("Retrieving versions of %s..." % project)
+            self.registry.load_versions(project, historical)
 
     def jsonify(self) -> dict:
         """Jsonifies a dataset."""
@@ -88,10 +127,8 @@ class Dataset:
 
         return JsonFileHandler()._jsonify(self)
 
-    def save_json(self, path: str = None):
+    def save_json(self, path: str = None) -> None:
         from file_handlers import JsonFileHandler
-
-        print('Saving results to %s' % path)
 
         # check that all necessary meta values have been set
         if not (self.name and self.version):
@@ -102,9 +139,10 @@ class Dataset:
         path = path or (self.name + '.json')
 
         # save to disk
+        print('Saving results to %s' % path)
         JsonFileHandler().save(self, path)
 
-    def get_project(self, **kwargs):
+    def get_project(self, **kwargs) -> Optional[Project]:
         """Gets a project matching all parameters or returns None."""
 
         # linear search function; potential for being slow...
@@ -119,7 +157,7 @@ class Dataset:
 
         return None
 
-    def _get_or_add_project(self, **kwargs):
+    def _get_or_add_project(self, **kwargs) -> Project:
         """Finds a matching project or adds a new one."""
         project = self.get_project(**kwargs)
         if not project:
