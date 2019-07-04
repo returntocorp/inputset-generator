85294e40fd10da4554a628ec2ce822176e0ad34c
Ben Fulton <fulton.benjamin@gmail.com>
2019-07-04 15:50:21 -0400

    Remove file save code from JsonLoader

--- inputset-generator/file_loaders/json.py
+++ inputset-generator/file_loaders/json.py
@@ -10,13 +10,6 @@ class JsonLoader(FileLoader):
             'r2c': self._parse_r2c
         }
 
-        self.jsonifiers = {
-            'GitRepo': self._jsonify_gitrepo,
-            'GitRepoCommit': self._jsonify_gitrepocommit,
-            'HttpUrl': self._jsonify_httpurl,
-            'PackageVersion': self._jsonify_packageversion
-        }
-
     def load(self, ds: Dataset, filepath: str,
              parser: str = 'r2c') -> None:
         """Loads a json file."""
@@ -48,7 +41,7 @@ class JsonLoader(FileLoader):
 
         # check that there's at least one input line
         # (caller will catch any exceptions)
-        data['inputs'][0]
+        _ = data['inputs'][0]
 
         # generate the projects and versions
         # note: to add more input types, simply expand the p_keys and
@@ -66,73 +59,3 @@ class JsonLoader(FileLoader):
                 version = project.get_or_add_version(**v_dict)
                 # set standardized version attr--for historical function
                 version.historical = v_dict.get('version', None)
-
-    def save(self, ds: Dataset, filepath: str) -> None:
-        """Writes a dataset to json. Unique to json filetype."""
-
-        # jsonify the dataset
-        try:
-            data = self._jsonify(ds)
-        except Exception:
-            raise Exception('Error jsonifying the dataset.')
-
-        # save the json to file
-        with open(filepath, 'w') as file:
-            json.dump(data, file)
-
-    def _jsonify(self, ds: Dataset) -> dict:
-        # add the dataset's metadata (common to all Datasets)
-        d = dict()
-        d['name'] = ds.name
-        d['version'] = ds.version
-        if ds.description: d['description'] = ds.description
-        if ds.readme: d['readme'] = ds.readme
-        if ds.author: d['author'] = ds.author
-        if ds.email: d['email'] = ds.email
-
-        # convert the dataset's projects and versions to inputs
-        # using the appropriate registered jsonifier
-        if ds.registry and ds.registry.name in ['github']:
-            # git projects are outputted as GitRepo or GitRepoCommit
-            if len(ds.projects[0].versions) == 0:
-                d['inputs'] = self.jsonifiers['GitRepo'](ds)
-            else:
-                d['inputs'] = self.jsonifiers['GitRepoCommit'](ds)
-        elif ds.registry and ds.registry.name in ['pypi', 'npm']:
-            # pypi/npm projects are outputted as PackageVersion
-            d['inputs'] = self.jsonifiers['PackageVersion'](ds)
-        else:
-            # other projects are outputted as HttpUrl
-            d['inputs'] = self.jsonifiers['HttpUrl'](ds)
-
-        return d
-
-    @staticmethod
-    def _jsonify_gitrepo(ds: Dataset) -> list:
-        return [{
-            'input_type': 'GitRepo',
-            'repo_url': getattr(project, 'repo_url')
-        } for project in ds.projects]
-
-    @staticmethod
-    def _jsonify_gitrepocommit(ds: Dataset) -> list:
-        return [{
-            'input_type': 'GitRepoCommit',
-            'repo_url': getattr(project, 'repo_url'),
-            'commit_hash': getattr(version, 'commit_hash')
-        } for project in ds.projects for version in project.versions]
-
-    @staticmethod
-    def _jsonify_httpurl(ds: Dataset) -> list:
-        return [{
-            'input_type': 'HttpUrl',
-            'url': getattr(project, 'url')
-        } for project in ds.projects]
-
-    @staticmethod
-    def _jsonify_packageversion(ds: Dataset) -> list:
-        return [{
-            'input_type': 'PackageVersion',
-            'package_name': getattr(project, 'package_name'),
-            'version': getattr(version, 'version')
-        } for project in ds.projects for version in project.versions]
