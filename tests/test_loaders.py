9d193528751b8cee5835bc9bed5c097b92894cc1
Ben Fulton <fulton.benjamin@gmail.com>
2019-07-07 23:31:59 -0400

    Update tests (still failing pypi weblist load test--correctly!)

--- tests/test_loaders.py
+++ tests/test_loaders.py
@@ -4,20 +4,29 @@ from structures import Dataset
 def test_json():
     # test github
     ds = Dataset('github')
+    ds.set_meta('test', '1.0')
     ds.load_file('../tests/files/git_repo.json')
+    test = ds.to_inputset()
     ds.load_file('../tests/files/git_repo_commit.json')
+    test = ds.to_inputset()
 
     # test npm
     ds = Dataset('npm')
+    ds.set_meta('test', '1.0')
     ds.load_file('../tests/files/name_version.json')
+    test = ds.to_inputset()
 
     # test pypi
     ds = Dataset('pypi')
+    ds.set_meta('test', '1.0')
     ds.load_file('../tests/files/name_version.json')
+    test = ds.to_inputset()
 
     # test vanilla
     ds = Dataset()
+    ds.set_meta('test', '1.0')
     ds.load_file('../tests/files/http_url.json')
+    test = ds.to_inputset()
 
     temp = 5
 
@@ -25,19 +34,27 @@ def test_json():
 def test_csv():
     # test github
     ds = Dataset('github')
+    ds.set_meta('test', '1.0')
     ds.load_file('../tests/files/git_urls_commits.csv')
+    test = ds.to_inputset()
 
     # test npm
     ds = Dataset('npm')
+    ds.set_meta('test', '1.0')
     ds.load_file('../tests/files/names_versions.csv')
+    test = ds.to_inputset()
 
     # test pypi
     ds = Dataset('pypi')
+    ds.set_meta('test', '1.0')
     ds.load_file('../tests/files/names_versions.csv')
+    test = ds.to_inputset()
 
     # test vanilla
     ds = Dataset()
-    ds.load_file('../tests/files/names_versions.csv')
+    ds.set_meta('test', '1.0')
+    ds.load_file('../tests/files/urls.csv')
+    test = ds.to_inputset()
 
     temp = 5
 
@@ -45,10 +62,14 @@ def test_csv():
 def test_weblist():
     # test github
     ds = Dataset('github')
+    ds.set_meta('test', '1.0')
     ds.load_weblist('top100starred')
+    test = ds.to_inputset()
 
     # test pypi
     ds = Dataset('pypi')
+    ds.set_meta('test', '1.0')
     ds.load_weblist('top5kyear')
+    test = ds.to_inputset()
 
     temp = 5
