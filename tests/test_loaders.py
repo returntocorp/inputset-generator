ea8980f4cb3596eccf7e7fc77bf4d539fbd8cd41
Ben Fulton <fulton.benjamin@gmail.com>
2019-07-09 15:47:24 -0400

    Change tests to try saving to file

--- tests/test_loaders.py
+++ tests/test_loaders.py
@@ -6,28 +6,28 @@ def test_json():
     # test github
     ds = Dataset('github')
     ds.set_meta('test', '1.0')
-    ds.load_file('../tests/files/git_repo.json')
+    ds.load_file('files/git_repo.json')
     test = ds.to_inputset()
-    ds.load_file('../tests/files/git_repo_commit.json')
-    test = ds.to_inputset()
+    ds.load_file('files/git_repo_commit.json')
+    ds.save('../test.json')
 
     # test npm
     ds = Dataset('npm')
     ds.set_meta('test', '1.0')
-    ds.load_file('../tests/files/name_version.json')
-    test = ds.to_inputset()
+    ds.load_file('files/name_version.json')
+    ds.save('../test.json')
 
     # test pypi
     ds = Dataset('pypi')
     ds.set_meta('test', '1.0')
-    ds.load_file('../tests/files/name_version.json')
-    test = ds.to_inputset()
+    ds.load_file('files/name_version.json')
+    ds.save('../test.json')
 
     # test vanilla
     ds = Dataset()
     ds.set_meta('test', '1.0')
-    ds.load_file('../tests/files/http_url.json')
-    test = ds.to_inputset()
+    ds.load_file('files/http_url.json')
+    ds.save('../test.json')
 
     temp = 5
 
@@ -36,26 +36,26 @@ def test_csv():
     # test github
     ds = Dataset('github')
     ds.set_meta('test', '1.0')
-    ds.load_file('../tests/files/git_urls_commits.csv')
-    test = ds.to_inputset()
+    ds.load_file('files/git_urls_commits.csv')
+    ds.save('../test.json')
 
     # test npm
     ds = Dataset('npm')
     ds.set_meta('test', '1.0')
-    ds.load_file('../tests/files/names_versions.csv')
-    test = ds.to_inputset()
+    ds.load_file('files/names_versions.csv')
+    ds.save('../test.json')
 
     # test pypi
     ds = Dataset('pypi')
     ds.set_meta('test', '1.0')
-    ds.load_file('../tests/files/names_versions.csv')
-    test = ds.to_inputset()
+    ds.load_file('files/names_versions.csv')
+    ds.save('../test.json')
 
     # test vanilla
     ds = Dataset()
     ds.set_meta('test', '1.0')
-    ds.load_file('../tests/files/urls.csv')
-    test = ds.to_inputset()
+    ds.load_file('files/urls.csv')
+    ds.save('../test.json')
 
     temp = 5
 
@@ -69,7 +69,7 @@ def test_weblist():
     ds.load_weblist('top1kstarred')
     ds.head(10)
     ds.get_project_versions('latest')
-    test = ds.to_inputset()
+    ds.save('../test.json')
 
     # test pypi
     ds = Dataset('pypi')
@@ -77,6 +77,6 @@ def test_weblist():
     ds.load_weblist('top5kyear')
     ds.head(10)
     ds.get_project_versions('latest')
-    test = ds.to_inputset()
+    ds.save('../test.json')
 
     temp = 5
