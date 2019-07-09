14a1086dadee1e8caa3ffb7d4f8e410946fdb871
Ben Fulton <fulton.benjamin@gmail.com>
2019-07-09 18:04:03 -0400

    First attempt at npm weblist downloader... 400 errors

--- inputset-generator/loaders/weblist/npm_loader.py
+++ inputset-generator/loaders/weblist/npm_loader.py
@@ -4,19 +4,12 @@ from loaders import Loader
 
 class NpmLoader(Loader):
     def __init__(self):
-        # add npm-specific weblists
-        # try this? https://www.npmjs.com/browse/depended
-        # https://stackoverflow.com/questions/34071621/query-npmjs-registry-via-api
-        # https://github.com/npm/download-counts
-        # https://github.com/npm/registry/blob/master/docs/REGISTRY-API.md#get-v1search
-        # https://stackoverflow.com/questions/48251633/list-all-public-packages-in-the-npm-registry
-
-        # idea: hit the /all endpoint to download all packages, then hit
-        # the /downloads/point with a comma-separated list of packages
-        # to get the downloads count?
-        # https://replicate.npmjs.com/_all_docs
-        # https://api.npmjs.org/downloads/point/last-year/npm,express,lodash,requests,bob
-        self.weblists = {}
+        self.weblists = {
+            'allbydownloads': {
+                'getter': self._get_allbydownloads,
+                'parser': self._parse_npm
+            }
+        }
 
     def load(self, ds: Dataset, name: str, **_) -> None:
         # load the data
@@ -24,3 +17,50 @@ class NpmLoader(Loader):
 
         # parse the data
         self.weblists[name]['parser'](ds, data)
+
+    @staticmethod
+    def _get_allbydownloads(api, **_) -> list:
+        # https://www.npmjs.com/browse/depended
+        # https://github.com/npm/download-counts
+        # https://stackoverflow.com/questions/34071621/query-npmjs-registry-via-api
+        # https://github.com/npm/registry/blob/master/docs/REGISTRY-API.md#get-v1search
+        # https://stackoverflow.com/questions/48251633/list-all-public-packages-in-the-npm-registry
+
+        # idea: hit the /all endpoint to download all packages, then hit
+        # the /downloads/point with a comma-separated list of packages
+        # to get the downloads count
+        # https://replicate.npmjs.com/_all_docs
+        # https://api.npmjs.org/downloads/point/last-year/npm,express,lodash,requests,bob
+
+        # load a list of projects
+        url = 'https://replicate.npmjs.com/_all_docs'
+        package_list = api.request(url)['rows']
+
+        # get the downloads count
+        packages = []
+        for start_at in range(0, len(package_list), 128):
+            # build the bulk request to the downloads count endpoint
+            # Note: Up to 128 packages may be requested at a time. See:
+            # https://github.com/npm/registry/blob/master/docs/download-counts.md#limits
+            names = ','.join([
+                p['id'] for p in package_list[start_at: start_at + 128]
+            ])
+            url = 'https://api.npmjs.org/downloads/point/last-month/%s' % names
+
+            # request the data from the api (cache/web)
+            data_dict = api.request(url)
+            packages.extend(list(data_dict.values()))
+
+        return packages
+
+    @staticmethod
+    def _parse_npm(ds: Dataset, data: list):
+        from structures.projects import NpmPackage
+
+        # map data keys to package keywords
+        uuids = {
+            'name': lambda p: p.package
+        }
+
+        # create the projects
+        ds.projects = [NpmPackage(uuids_=uuids, **d) for d in data]
