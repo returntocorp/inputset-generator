0cc5450fe74d601ad282aa54cc7741b8a853238b
Ben Fulton <fulton.benjamin@gmail.com>
2019-07-15 09:54:36 -0400

    Ensure project/version map keys match

--- inputset-generator/structures/__init__.py
+++ inputset-generator/structures/__init__.py
@@ -1,3 +1,8 @@
 from .dataset import Dataset
 from .projects import Project, DefaultProject, project_map
-from .versions import Version, DefaultVersion
+from .versions import Version, DefaultVersion, version_map
+
+
+# check to ensure project/version map keys match
+assert project_map.keys() == version_map.keys(), (
+    'Project_map and version_map must have the same map keys.')
