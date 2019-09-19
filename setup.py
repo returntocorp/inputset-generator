a3981cb73f982b95119ff67bd06ea2e8a1b1a6bf
Ben Fulton <fulton.benjamin@gmail.com>
2019-09-19 14:33:03 -0400

    Update to version 0.2.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md', 'r') as file:
 with open('requirements.txt') as file:
     requirements = file.read().splitlines()
 
-version = '0.2.4'
+version = '0.2.5'
 
 setup(name='r2c-inputset-generator',
       version=version,
